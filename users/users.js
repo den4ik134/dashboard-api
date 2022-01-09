947dd88732bfc7d53b6c40617e91bbf990cee74c
Denys Bilyk <bilko.dp@gmail.com>
2022-01-09 20:20:44 +0200

    startup project

---
+++ users/users.js
@@ -0,0 +1,19 @@
+import express from "express";
+
+
+const userRouter = express.Router();
+
+userRouter.use((req, res, next) => {
+    console.log('Users handler');
+    next();
+});
+
+userRouter.post('/login', (req, res) => {
+    res.send('Login')
+});
+
+userRouter.post('/register', (req, res) => {
+    res.send('Register')
+});
+
+export {userRouter};
