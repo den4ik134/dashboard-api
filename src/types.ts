c12b758bcbfb11525734e36df677cda73af97ec0
Denys Bilyk <bilko.dp@gmail.com>
2022-01-16 22:08:51 +0200

    Added prisma DB

--- src/types.ts
+++ src/types.ts
@@ -5,4 +5,5 @@ export const TYPES = {
 	UsersService: Symbol.for('UsersService'),
 	ExeptionFilter: Symbol.for('ExeptionFilter'),
 	ConfigService: Symbol.for('ConfigService'),
+	PrismaService: Symbol.for('PrismaService'),
 };
